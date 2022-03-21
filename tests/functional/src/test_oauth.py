db7b2b81b567a5038d7e4e24c41f7dcabd91a0e6
BernarBerdikul <dabeerman32215@gmail.com>
2022-03-22 05:49:53 +0600

    fix documentation & add test for oauth

---
+++ tests/functional/src/test_oauth.py
@@ -0,0 +1,16 @@
+import http
+import pytest
+from core.oauth_service import register_social_account
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_api():
+    email: str = "test@gmail.com"
+    username: str = "baltazar"
+    data, code = register_social_account(
+        social_name="facebook", social_id="7777777", email=email, username=username
+    )
+    assert data.get("message") == f"Logged in as {username} - {email}"
+    assert type(data.get("access_token")) == str
+    assert code == http.HTTPStatus.OK
